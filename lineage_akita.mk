d4e8195c5e54948bc213d39a9a90d701c97c5ea1
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-22 21:22:09 +0530

    Add initial Lineage product makefiles
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6

---
+++ lineage_akita.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Lineage stuff.
+TARGET_DISABLE_EPPE := true
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/akita/aosp_akita.mk)
+$(call inherit-product, device/google/zuma/lineage_common.mk)
+$(call inherit-product, device/google/akita/device-lineage.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 8a
+PRODUCT_NAME := lineage_akita
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=akita \
+    PRIVATE_BUILD_DESC="akita-user 14 UD2A.240505.001.W1 11715222 release-keys"
+
+BUILD_FINGERPRINT := google/akita/akita:14/UD2A.240505.001.W1/11715222:user/release-keys
