3561c89a79c65087cd782e44f8c74f2dd34a6d10
lahaina <nattolecats@gmail.com>
2024-05-20 17:50:36 +0900

    akita: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Remove conflicts
+rm -rf vendor/gms/product/packages/privileged_apps/AICorePrebuilt/Android.mk
+rm -rf vendor/gms/product/packages/privileged_apps/DeviceIntelligenceNetworkPrebuilt/Android.mk
